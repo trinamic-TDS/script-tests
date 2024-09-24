eaf2ceeee4120256f5385cdb4a222ed43009e5a6
Tushar Shiralkar <tushardinesh.shiralkar@analog.com>
2024-09-24 15:18:38 +0200

    Corrected clang-checker

--- clang-checker.sh
+++ clang-checker.sh
@@ -1,37 +1,26 @@
 #!/bin/bash
-# echo "Aborting commit. Please manually review and commit changes."
-# Run clang-format on all staged C/C++ files
-# Finds all staged .c, .h, .cpp, .hpp files and runs clang-format on them
-# Allows us to read user input below, assigns stdin to keyboard
-exec < /dev/tty
 
-FILES=$(git diff --cached --name-only --diff-filter=ACM | grep -E '\.(c|cpp|h|hpp)$')
+# Finds all staged .c, .h, .cpp, .hpp files and runs clang-format on them
+FILES=$(git diff --name-only --diff-filter=ACM | grep -E '\.(c|cpp|h|hpp)$')
 
 if [ -n "$FILES" ]; then
+    echo "Running clang-format on the following files:"
+    echo "$FILES"
+    
     # Run clang-format and apply changes
-    echo "Running clang-format on staged files..."
     clang-format -i $FILES
-
+    
     # Check if clang-format made any changes
     MODIFIED_FILES_COUNT=$(git diff --name-only --diff-filter=M $FILES | wc -l)
-
+    
     if [ $MODIFIED_FILES_COUNT != 0 ]; then
         echo "clang-format has made changes to the following files:"
         git diff --name-only --diff-filter=M $FILES
-
-        # Prompt the user for acceptance
-        read -p "Do you want to accept clang-format changes and include them in the commit? (y/n): "
-        
-        if [ "$REPLY" == "y" ] ;then
-            # Stage the formatted files
-            git add $FILES
-        else
-            echo "Aborting commit. Please manually review and commit changes."
-            exit 1
-        fi
+        echo "::error::Please apply clang-format changes before committing."
+        exit 1
     else
         echo "No formatting changes needed."
     fi
+else
+    echo "No C/C++ files to check."
 fi
-
-# Proceed with the commit
